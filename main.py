c9309f943ea1dbca3756c01cdfef44ccb6ec2ad1
laura <l.restrepop@uniandes.edu.co>
2024-05-20 13:20:29 -0500

    Add: object detection endpoint

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from PIL import ImageGrab
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -7,21 +9,35 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/screenshot", methods=["POST", "GET"]) 
-def screenshot():
+@app.route("/detect", methods=["POST", "GET"]) 
+def detetc():
     if request.method == "POST":
         data = request.get_json()
         ss_key = "screenshot_name"
         try:
             ss_name = data[ss_key]
             screenshot = ImageGrab.grab()
-            screenshot.save("{fname}.png".format(fname=ss_name))
+            ss_full_name = "{fname}.png".format(fname=ss_name)
+            screenshot.save(ss_full_name)
             screenshot.close()
-            result = "Screenshot with name '{fname}' created".format(fname=ss_name)
-            return result, 201
+
+            headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZjg4ZWMyNDUtOGNhMi00MGE2LWE4NzEtMWM5ZmU0ZmE5YWNlIiwidHlwZSI6ImFwaV90b2tlbiJ9.BV9MP-DFCnUdMlrkkN9E-nWR_ThUmTlI11-eBlguDRM"}
+            url = "https://api.edenai.run/v2/image/object_detection"
+            data = {
+                "providers": "amazon",
+                "fallback_providers": "google"
+            }
+            files = {'file': open(ss_full_name, 'rb')}
+
+            response = requests.post(url, data=data, files=files, headers=headers)
+            result = json.loads(response.text)
+            items = result['amazon']['items']
+            labels = [i['label'] for i in items]
+        
+            return labels, 200
         except:
             if ss_key not in data:
                 error = "The body of your request did not have screennshot_name key, please try again"
                 return error, 400
     elif request.method == "GET": 
-        return render_template("screenshot.html")
+        return render_template("detect.html")
